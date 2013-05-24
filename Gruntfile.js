5837c13d4d59b51067f95c51f36e5deb11b69485
Matt Colyer <matt@colyer.name>
2013-05-23 17:11:10 -0700

    Add grunt jasmine task for automated testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,6 @@ module.exports = function(grunt) {
       },
       test: {
         expand: true,
-        flatten: true,
         cwd: 'test/src/',
         src: ['**/*.coffee'],
         dest: 'test/lib/',
@@ -45,6 +44,21 @@ module.exports = function(grunt) {
         files: ['test/src/**/*.coffee'],
         tasks: ['coffee:test']
       }
+    },
+
+    jasmine: {
+      src: '<%= pkg.name %>.js',
+      options: {
+        specs: ['test/lib/tourSpec.js', 'test/lib/tip/baseSpec.js', 'test/lib/tip/qtipSpec.js', 'test/lib/tip/bootstrapSpec.js'],
+        helpers: 'test/lib/**/*Helper.js',
+        vendor: ['test/vendor/javascripts/jquery-1.9.1.js',
+          'test/vendor/javascripts/underscore-1.4.4.js',
+          'test/vendor/javascripts/backbone-1.0.0.js',
+          'test/vendor/javascripts/jquery.qtip.js',
+          'test/vendor/javascripts/jquery-ui-1.10.2.custom.js',
+          'test/vendor/javascripts/jasmine-jquery.js'
+        ]
+      }
     }
   });
 
@@ -52,6 +66,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
+  grunt.registerTask('test', ['coffee', 'jasmine']);
   grunt.registerTask('default', ['concat', 'coffee', 'uglify']);
 };
