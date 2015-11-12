d6e1075b0458fc01567997076af881967da38456
Daniel Gasser <info@pc-shooter.ch>
2015-11-12 11:40:56 +0100

    fixed again: the parameter should be the step to go, not the step to be added/substracted to/from the current step

--- tourist.js
+++ tourist.js
@@ -926,15 +926,11 @@
       }
     };
 
-    Tour.prototype.showStep = function(index) {
-      var currentStep;
+    Tour.prototype.showStep = function(show_step) {
+      var currentStep, index;
       currentStep = this._teardownCurrentStep();
       if (currentStep) {
-          if (index < currentStep.index) {
-              index = currentStep.index - index;
-          } else {
-              index = currentStep.index + index;
-          }
+          index = show_step;
       }
       if (index < this.options.steps.length && !isNaN(parseInt(index, 10))) {
         return this._showStep(this.options.steps[index], index);
